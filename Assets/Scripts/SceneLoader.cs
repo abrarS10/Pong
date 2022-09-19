3f3bf21473716cb77001d5f21da3a630dc43ed50
abrar_sajeel <abrarsajeel@gmail.com>
2022-09-19 13:14:23 +0500

    Changed scene sequence

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,12 +8,12 @@ public class SceneLoader : MonoBehaviour
     public void LoadGame(){
         Debug.Log("start again");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
         Time.timeScale = 1f;
     }
 
     public void LoadMenu(){
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 
     public void QuitGame(){
