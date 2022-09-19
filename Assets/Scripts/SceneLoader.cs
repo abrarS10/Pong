78acc8c7df4c714f0e81d4ce0c81aa6db02a541c
abrar_sajeel <abrarsajeel@gmail.com>
2022-09-19 11:27:16 +0500

    Completed pause menu

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,6 +8,11 @@ public class SceneLoader : MonoBehaviour
     public void LoadGame(){
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMenu(){
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame(){
